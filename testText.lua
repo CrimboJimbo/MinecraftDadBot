f95d23ee0a873adb838ec683626a64a06cd0e488
FearlessBirb <61lando@gmail.com>
2024-09-20 22:07:37 -0400

    yay

--- testText.lua
+++ testText.lua
@@ -5,6 +5,10 @@ local inputMod = string.gsub(input, " ", "_")
 local txt = request.readAll()
 local a,b
 a,b = string.find(txt,inputMod)
-a,b = string.find(txt,"%b{}",b+1)
-print(string.sub(txt,a,b))
+if a == nil or b == nil then
+    error("idiot")
+else
+    a,b = string.find(txt,"%b{}",b+1)
+    print(string.sub(txt,a+1,b-1))
+end
 request.close()
