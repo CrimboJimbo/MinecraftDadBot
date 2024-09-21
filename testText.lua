baf4ee661b8dabe22e2dd8d45a2e91c6b6fcea46
FearlessBirb <61lando@gmail.com>
2024-09-20 21:25:02 -0400

    ah

--- testText.lua
+++ testText.lua
@@ -1,3 +1,4 @@
 local request = http.get("https://raw.githubusercontent.com/CrimboJimbo/MinecraftDadBot/refs/heads/main/Text%20Test.txt")
-print(request.string.find([]))
+local a,b = string.find(request,"[iron_golem]")
+print(a,b)
 request.close()
