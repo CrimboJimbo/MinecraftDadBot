594e4e1c7da46e41bcce13dcdfa2deea06f53442
CrimboJimbo <minecraftian98787679@gmail.com>
2024-09-22 13:29:18 -0400

    Testing calling funtions from TxT file

--- testText.lua
+++ testText.lua
@@ -3,13 +3,17 @@ local input = read() --Replace with input from chat
 local request = http.get("https://raw.githubusercontent.com/CrimboJimbo/MinecraftDadBot/refs/heads/main/Dad%20Commands.txt")
 local inputMod = string.lower(input)
 local txt = request.readAll()
-local a,b
+local a,b,c
+function TestFunc()
+    print('Yippie!')
+end
 print(inputMod)
 a,b = string.find(txt,inputMod)
 if a == nil or b == nil then
     error("idiot")
 else
     a,b = string.find(txt,"%b{}",b+1)
-    print(string.sub(txt,a+1,b-1)) --Replace with output to chat
+    c = string.sub(txt,a+1,b-1)
+    _G[c]()
 end
 request.close()
