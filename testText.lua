4f2b1abc81925af5bd15ba6f6970507345051ba4
CrimboJimbo <minecraftian98787679@gmail.com>
2024-09-22 17:44:15 -0400

    scrim scrum no run

--- testText.lua
+++ testText.lua
@@ -4,6 +4,7 @@ local request = http.get("https://raw.githubusercontent.com/CrimboJimbo/Minecraf
 local inputMod = string.lower(input)
 local txt = request.readAll()
 local a,b,c
+-- print(txt)
 function TestFunc()
     print('Yippie!')
 end
@@ -14,8 +15,12 @@ if a == nil or b == nil then
     error("idiot")
 else
     a,b = string.find(txt,"%b{}",b+1)
+    print(a,b)
     c = string.sub(txt,a+1,b-1)
-    print(c)
-    print(assert(c)())
+    local Scrum = load("return function() "..c.." end",nil,nil,_G)
+    local Ok, Scrim = pcall(Scrum)
+    print(Ok)
+    Scrim()
+    -- _G[c]()
 end
 request.close()
