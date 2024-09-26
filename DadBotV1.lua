339988f7c856213c9f9d1ec7885050181f11f66c
crimbojimbo <minecraftian98787679@gmail.com>
2024-09-26 11:51:29 -0400

    Formating and txt updates

--- DadBotV1.lua
+++ DadBotV1.lua
@@ -1,6 +1,6 @@
 local Dad = peripheral.wrap("right")
 local newMessage, ss, se, event, username, message, uuid, isHidden
-local imCheck = { "i'm ", "i am ", "im " }
+local imCheck = {"i'm ", "i am ", "im "}
 Dad.sendMessage("Hello son!", "DadBot", "<>", "&e")
 
 function _G.DadChat(m, t)
@@ -13,8 +13,8 @@ function _G.DadChat(m, t)
 end
 
 function _G.YNChecker(message)
-    local yCheck = { "y", "yes", "yup", "you bet" }
-    local nCheck = { "n", "no", "nope", "nada", "i do not", "i don't" }
+    local yCheck = {"y", "yes", "yup", "you bet"}
+    local nCheck = {"n", "no", "nope", "nada", "i do not", "i don't"}
     for k, v in pairs(yCheck) do
         if CheckMessage(message, v) then
             return true
@@ -28,7 +28,7 @@ function _G.YNChecker(message)
 end
 
 function _G.CheckMessage(m, s)
-    local a,b
+    local a, b
     m = string.lower(m)
     a, b = string.find(m, s)
     if a == nil then
@@ -40,14 +40,14 @@ end
 
 function _G.DadCommandChecker()
     local request = http.get(
-    "https://raw.githubusercontent.com/CrimboJimbo/MinecraftDadBot/refs/heads/main/Dad%20Commands.txt")
+        "https://raw.githubusercontent.com/CrimboJimbo/MinecraftDadBot/refs/heads/main/Dad%20Commands.txt")
     event, username, message, uuid, isHidden = os.pullEvent("chat")
     local inputMod = string.lower(message)
     local txt = request.readAll()
     local a, b, c
     a, b = string.find(txt, inputMod)
     if a == nil or b == nil then
-        DadChat("I didn't understand that command. Say \"Hey Dad\" to try again.") --Formerly error("idiot")
+        DadChat("I didn't understand that command. Say \"Hey Dad\" to try again.") -- Formerly error("idiot")
     else
         a, b = string.find(txt, "%b{}", b + 1)
         print(a, b)
@@ -69,27 +69,27 @@ function _G.SaveNickNames()
     event, username, message, uuid, isHidden = os.pullEvent("chat")
     local tempNameHolder = message
     local names = file.readAll()
-    local a, b, c, d, e = nil,nil,nil,nil,nil
+    local a, b, c, d, e = nil, nil, nil, nil, nil
     if names == nil then
-        file.writeLine("["..username.."]".."{"..message.."}")
+        file.writeLine("[" .. username .. "]" .. "{" .. message .. "}")
         os.sleep(1)
-        DadChat("Alright, I'll call you "..message.."!")
+        DadChat("Alright, I'll call you " .. message .. "!")
     else
         a, b = string.find(names, username)
         if a == nil or b == nil then
-            file.writeLine("["..username.."]".."{"..message.."}")
+            file.writeLine("[" .. username .. "]" .. "{" .. message .. "}")
         else
             a, b = string.find(names, "%b{}", b + 1)
             c = string.sub(names, a + 1, b - 1)
             os.sleep(1)
-            DadChat("You already the the nickname: \""..c.."\"")
+            DadChat("You already the the nickname: \"" .. c .. "\"")
             os.sleep(1)
             DadChat("Do you want to change it?")
             event, username, message, uuid, isHidden = os.pullEvent("chat")
             if YNChecker(message) then
                 d = string.sub(names, 1, a)
                 e = string.sub(names, b, string.len(names))
-                names = d..tempNameHolder..e
+                names = d .. tempNameHolder .. e
                 file.close()
                 fs.delete("MinecraftDadBot/nicknames.txt")
                 sleep(1)
@@ -97,7 +97,6 @@ function _G.SaveNickNames()
                 file.close()
                 file = fs.open("MinecraftDadBot/nicknames.txt", "r+")
                 file.write(names)
-                --Temp
                 os.sleep(1)
                 DadChat("Name updated!")
             end
@@ -126,25 +125,25 @@ function _G.AskDad()
     end
 end
 
-function _G.DadWiki() --Wiki function, derived from AskDad()
+function _G.DadWiki() -- Wiki function, derived from AskDad()
     DadChat("What would you like to know?")
     local qq = 1
     while qq == 1 do
-        event, username, message, uuid, isHidden = os.pullEvent("chat") --Input from player
+        event, username, message, uuid, isHidden = os.pullEvent("chat") -- Input from player
         local request = http.get(
-        "https://raw.githubusercontent.com/CrimboJimbo/MinecraftDadBot/refs/heads/main/Text%20Test.txt")
-        local inputMod = string.gsub(message, " ", "_") --Converts chat input into wiki item format
-        local txt = request.readAll()                   --Converts wiki site file into a readable string
-        local a, b = string.find(txt, inputMod)         --Location of wiki item within string
-        if a == nil or b == nil then                    --If wiki item doesn't match anything in wiki list
-            DadChat("I have no Idea.")                  --Formerly error("idiot")
-        else                                            --If wiki item is in wiki list
-            a, b = string.find(txt, "%b{}", b + 1)      --Find wiki item's description
-            DadChat(string.sub(txt, a + 1, b - 1))      --Chats description
+            "https://raw.githubusercontent.com/CrimboJimbo/MinecraftDadBot/refs/heads/main/Text%20Test.txt")
+        local inputMod = string.gsub(message, " ", "_") -- Converts chat input into wiki item format
+        local txt = request.readAll() -- Converts wiki site file into a readable string
+        local a, b = string.find(txt, inputMod) -- Location of wiki item within string
+        if a == nil or b == nil then -- If wiki item doesn't match anything in wiki list
+            DadChat("I have no Idea.") -- Formerly error("idiot")
+        else -- If wiki item is in wiki list
+            a, b = string.find(txt, "%b{}", b + 1) -- Find wiki item's description
+            DadChat(string.sub(txt, a + 1, b - 1)) -- Chats description
         end
         request.close()
         os.sleep(1)
-        DadChat("Do you have any other questions?") --Below checks if there are more questions and exits if not
+        DadChat("Do you have any other questions?") -- Below checks if there are more questions and exits if not
         event, username, message, uuid, isHidden = os.pullEvent("chat")
         if YNChecker(message) then
             DadChat("What would you like to know?")
@@ -159,50 +158,51 @@ end
 
 function _G.Calculator(i)
     while true do
-        local temp1,temp2, a,b,c,d,e
-        a,b = string.find(i,"%b()")
+        local temp1, temp2, a, b, c, d, e
+        a, b = string.find(i, "%b()")
         if a == nil then
             i = Calc(i)
             return i
         else
-            c= string.sub(i,a+1,b-1)
-            d,e = string.find(c,"%b()")
-            if d~=nil then
+            c = string.sub(i, a + 1, b - 1)
+            d, e = string.find(c, "%b()")
+            if d ~= nil then
                 c = Calculator(c)
             end
-            temp1 = string.sub(i,1,a-1)
-            temp2 = string.sub(i,b+1)
+            temp1 = string.sub(i, 1, a - 1)
+            temp2 = string.sub(i, b + 1)
             c = Calc(c)
-            i = temp1..c..temp2
+            i = temp1 .. c .. temp2
         end
     end
 end
+
 function _G.Calc(input)
     local digit, cType
     local cResult = 0
-    _, _, cResult, input = string.find(input,"([+-]?%d*)(.*)")
+    _, _, cResult, input = string.find(input, "([+-]?%d*)(.*)")
     while input ~= "" do
-        cType = string.sub(input,1,1)
-        input = string.sub(input,2)
-        _, _, digit, input = string.find(input,"([+-]?%d*)(.*)")
-        if cType == "+"then
-            cResult = cResult+digit
+        cType = string.sub(input, 1, 1)
+        input = string.sub(input, 2)
+        _, _, digit, input = string.find(input, "([+-]?%d*)(.*)")
+        if cType == "+" then
+            cResult = cResult + digit
             os.sleep(1)
             DadChat("Hmm... Is that Right?")
-        elseif cType == "-"then
-            cResult = cResult-digit
+        elseif cType == "-" then
+            cResult = cResult - digit
             os.sleep(1)
             DadChat("No no no, I need to do that one again...")
-        elseif cType == "*"then
-            cResult = cResult*digit
+        elseif cType == "*" then
+            cResult = cResult * digit
             os.sleep(1)
             DadChat("I need a new piece of paper...")
-        elseif cType == "/" or cType == "\\"then
-            cResult = cResult/digit
+        elseif cType == "/" or cType == "\\" then
+            cResult = cResult / digit
             os.sleep(1)
             DadChat("Huh. That can't be right...")
-        elseif cType == "^"then
-            cResult = cResult^digit
+        elseif cType == "^" then
+            cResult = cResult ^ digit
             os.sleep(1)
             DadChat("I think that's how you do that.")
         end
@@ -217,8 +217,8 @@ function _G.DadMath()
     os.sleep(1)
     DadChat("Alright, lets see what I can do.")
     os.sleep(1)
-    local t,t2
-    t = string.find(message,"[^%d%-%+/\\%*%^%(%)]")
+    local t, t2
+    t = string.find(message, "[^%d%-%+/\\%*%^%(%)]")
     if t == nil then
         local result = Calculator(message)
         os.sleep(1)
@@ -228,12 +228,12 @@ function _G.DadMath()
     end
 end
 
---#region
---Prime Dad Loop
+-- #region
+-- Prime Dad Loop
 while true do
     event, username, message, uuid, isHidden = os.pullEvent("chat")
     message = string.lower(message)
-    ss,se = nil,nil
+    ss, se = nil, nil
     for k, v in pairs(imCheck) do
         if ss == nil or ss == "" then
             ss, se = string.find(message, v)
@@ -262,13 +262,13 @@ while true do
     if CheckMessage(message, "hey dad") then
         local file = fs.open("MinecraftDadBot/nicknames.txt", "r")
         local names = file.readAll()
-        local a,b,c
+        local a, b, c
         if names ~= nil then
             a, b = string.find(names, username)
             if a ~= nil then
                 a, b = string.find(names, "%b{}", b + 1)
                 c = string.sub(names, a + 1, b - 1)
-                DadChat("Yes "..c.."?")
+                DadChat("Yes " .. c .. "?")
             end
         else
             DadChat("Yes son?")
@@ -276,4 +276,4 @@ while true do
         DadCommandChecker()
     end
 end
---#endregion
+-- #endregion
