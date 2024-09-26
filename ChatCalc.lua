7788d3b93c8f1309490b79b860db71c7ebe4db45
CrimboJimbo <minecraftian98787679@gmail.com>
2024-09-25 20:18:09 -0400

    DadMath can can handle non-nested "()" now

--- ChatCalc.lua
+++ ChatCalc.lua
@@ -1,42 +1,77 @@
-local arg = { ... }
-local d1, d2, newArg, mathInput, cOut
-_, _, d1, mathInput, d2, newArg = string.find(arg[1], "([+-]?%d*)(.)([+-]?%d+)(.*)")
-if mathInput == "+" then
-    cOut = (tonumber(d1) + tonumber(d2))
-elseif mathInput == "-" then
-    cOut = (tonumber(d1) - tonumber(d2))
-elseif mathInput == "*" then
-    cOut = (tonumber(d1) * tonumber(d2))
-elseif mathInput == "/" then
-    cOut = (tonumber(d1) / tonumber(d2))
-end
--- print("Input num 1: "..d1)
--- print("Input Math Sym: "..mathInput)
--- print("Input num 2: "..d2)
--- print("Current Output Value: "..cOut)
--- print("Remaining Args: "..newArg)
-while true do
-    if newArg == nil then
-        break
+write(">")
+local spininput = read()
+local t1,t2,t3
+local Result = 0
+-- _, _, d1, mathInput, d2, newArg = string.find(arg[1], "([+-]?%d*)(.)([+-]?%d+)(.*)")
+
+function _G.Calculatorss(i)
+    while true do
+        local temp1,temp2, a,b,c
+        a,b = string.find(i,"%b()")
+        if a == nil then
+            i = Calc(i)
+            return i
+        else
+            c= string.sub(i,a+1,b-1)
+            temp1 = string.sub(i,1,a-1)
+            temp2 = string.sub(i,b+1)
+            c = Calc(c)
+            i = temp1..c..temp2
+        end
     end
-    _, _, mathInput, d2, newArg = string.find(newArg, "(.)([+-]?%d+)(.*)")
-    -- print("in loop d2:"..d2)
-    -- print("cOut pre op: "..cOut)
-    if mathInput == "+" then
-        -- print("d2: "..d2)
-        -- print("cOut: "..cOut)
-        cOut = (cOut + tonumber(d2))
-        -- print("MATH cOut: "..cOut)
-    elseif mathInput == "-" then
-        cOut = (cOut - tonumber(d2))
-    elseif mathInput == "*" then
-        cOut = (cOut * tonumber(d2))
-    elseif mathInput == "/" then
-        cOut = (cOut / tonumber(d2))
-    else
-        break
-    end
-    -- print("new math: "..mathInput)
-    -- print("cOut: "..cOut)
 end
-print("Result: " .. cOut)
+
+function _G.Calcss(input)
+    local digit, cType
+    local cResult = 0
+    _, _, cResult, input = string.find(input,"([+-]?%d*)(.*)")
+    while input ~= "" do
+        cType = string.sub(input,1,1)
+        input = string.sub(input,2)
+        _, _, digit, input = string.find(input,"([+-]?%d*)(.*)")
+        if cType == "+"then
+            cResult = cResult+digit
+        elseif cType == "-"then
+            cResult = cResult-digit
+        elseif cType == "*"then
+            cResult = cResult*digit
+        elseif cType == "/" or cType == "\\"then
+            cResult = cResult/digit
+        elseif cType == "^"then
+            cResult = cResult^digit
+        end
+    end
+    return cResult
+end
+
+Result = Calculator(spininput)
+print(Result)
+
+
+
+-- if mathInput == "+" then
+--     cOut = (tonumber(d1) + tonumber(d2))
+-- elseif mathInput == "-" then
+--     cOut = (tonumber(d1) - tonumber(d2))
+-- elseif mathInput == "*" then
+--     cOut = (tonumber(d1) * tonumber(d2))
+-- elseif mathInput == "/" then
+--     cOut = (tonumber(d1) / tonumber(d2))
+-- end
+-- while true do
+--     if newArg == nil then
+--         break
+--     end
+--     _, _, mathInput, d2, newArg = string.find(newArg, "(.)([+-]?%d+)(.*)")
+--     if mathInput == "+" then
+--         cOut = (cOut + tonumber(d2))
+--     elseif mathInput == "-" then
+--         cOut = (cOut - tonumber(d2))
+--     elseif mathInput == "*" then
+--         cOut = (cOut * tonumber(d2))
+--     elseif mathInput == "/" then
+--         cOut = (cOut / tonumber(d2))
+--     else
+--         break
+--     end
+-- end
--- DadBotV1.lua
+++ DadBotV1.lua
@@ -157,42 +157,53 @@ function _G.DadWiki() --Wiki function, derived from AskDad()
     end
 end
 
+function _G.Calculator(i)
+    while true do
+        local temp1,temp2, a,b,c
+        a,b = string.find(i,"%b()")
+        if a == nil then
+            i = Calc(i)
+            return i
+        else
+            c= string.sub(i,a+1,b-1)
+            temp1 = string.sub(i,1,a-1)
+            temp2 = string.sub(i,b+1)
+            c = Calc(c)
+            i = temp1..c..temp2
+        end
+    end
+end
+function _G.Calc(input)
+    local digit, cType
+    local cResult = 0
+    _, _, cResult, input = string.find(input,"([+-]?%d*)(.*)")
+    while input ~= "" do
+        cType = string.sub(input,1,1)
+        input = string.sub(input,2)
+        _, _, digit, input = string.find(input,"([+-]?%d*)(.*)")
+        if cType == "+"then
+            cResult = cResult+digit
+        elseif cType == "-"then
+            cResult = cResult-digit
+        elseif cType == "*"then
+            cResult = cResult*digit
+        elseif cType == "/" or cType == "\\"then
+            cResult = cResult/digit
+        elseif cType == "^"then
+            cResult = cResult^digit
+        end
+    end
+    return cResult
+end
+
 function _G.DadMath()
-    local cal = true
-    local i = 0
     DadChat("Lets do some Math")
     os.sleep(1)
     DadChat("What would you like to know?")
-    ev, use, mess, uuid, hide = os.pullEvent("chat")
-    local d1, d2, newArg, mathInput, cOut
-    _, _, d1, mathInput, d2, newArg = string.find(mess, "([+-]?%d*)(.)([+-]?%d+)(.*)")
-    if mathInput == "+" then
-        cOut = (tonumber(d1) + tonumber(d2))
-    elseif mathInput == "-" then
-        cOut = (tonumber(d1) - tonumber(d2))
-    elseif mathInput == "*" then
-        cOut = (tonumber(d1) * tonumber(d2))
-    elseif mathInput == "/" then
-        cOut = (tonumber(d1) / tonumber(d2))
-    end
-    while true do
-        if newArg == nil then
-            break
-        end
-        _, _, mathInput, d2, newArg = string.find(newArg, "(.)([+-]?%d+)(.*)")
-        if mathInput == "+" then
-            cOut = (cOut + tonumber(d2))
-        elseif mathInput == "-" then
-            cOut = (cOut - tonumber(d2))
-        elseif mathInput == "*" then
-            cOut = (cOut * tonumber(d2))
-        elseif mathInput == "/" then
-            cOut = (cOut / tonumber(d2))
-        else
-            break
-        end
-    end
-    DadChat("Result: " .. cOut)
+    event, username, message, uuid, isHidden = os.pullEvent("chat")
+    os.sleep(1)
+    local result = Calculator(message)
+    DadChat("Result: " .. result)
 end
 
 --#region
